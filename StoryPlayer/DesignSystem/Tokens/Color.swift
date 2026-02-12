ee80c11e3dc9e40c763826cd885f0dd56bf6c50c
adam-regan <39152610+adam-regan@users.noreply.github.com>
2026-02-12 19:01:23 +0000

    Initial project setup with loading screen

---
+++ StoryPlayer/DesignSystem/Tokens/Color.swift
@@ -0,0 +1,24 @@
+//
+//  Color.swift
+//  StoryPlayer
+//
+//  Created by Adam Regan on 12/02/2026.
+//
+
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+    let companyColor = Color("CompanyColor")
+    let companyText = Color("CompanyTextColor")
+    let pallete1 = Color("Pallete1Color")
+    let pallete2 = Color("Pallete2Color")
+    let pallete3 = Color("Pallete3Color")
+    let pallete4 = Color("Pallete4Color")
+    let pallete5 = Color("Pallete5Color")
+    let pallete6 = Color("Pallete6Color")
+    let text = Color("TextColor")
+}
