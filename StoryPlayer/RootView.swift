f2916a87cbd2d4068e2ec3901ff5dcf559200e5b
adam-regan <39152610+adam-regan@users.noreply.github.com>
2026-04-28 15:06:27 +0100

    Added launch screen

--- StoryPlayer/RootView.swift
+++ StoryPlayer/RootView.swift
@@ -1,28 +1,30 @@
-//
-//  RootView.swift
-//  StoryPlayer
-//
-//  Created by Adam Regan on 13/02/2026.
-//
+    //
+    //  RootView.swift
+    //  StoryPlayer
+    //
+    //  Created by Adam Regan on 13/02/2026.
+    //
 
 import SwiftUI
 
 struct RootView: View {
-    enum RootState {
-        case loading, main
-    }
-
-    @State private var rootState: RootState = .loading
+    @AppStorage("colorTheme") private var isDarkMode = false
+    @StateObject private var audioViewModel: AudioViewModel = .init(audioPlayer: .init())
+    @State private var selectedTab: Tabs = .library
 
     var body: some View {
-        switch rootState {
-        case .loading:
-            SplashScreenView(continueAction: {
-                rootState = .main
-            })
-        case .main:
-            MainTabView()
+        ZStack {
+            switch selectedTab {
+                case .library:
+                    LibraryView()
+                case .settings:
+                    SettingsView()
+            }
+
+            CustomTabBarView(selectedTab: $selectedTab)
         }
+        .environmentObject(audioViewModel)
+        .preferredColorScheme(isDarkMode ? .dark : .light)
     }
 }
 
